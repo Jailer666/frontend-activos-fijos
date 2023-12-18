ae3be9e57b4396887a8b62abf11e8db1296fece9
jailer666 <jailerham@gmail.com>
2023-12-18 19:28:47 -0400

    vista arma modificada

--- src/views/armas/Armas.vue
+++ src/views/armas/Armas.vue
@@ -3,10 +3,10 @@
     <table class="table table-bordered">
       <thead>
         <tr>
+          <th>#</th>
           <th rowspan="2">FECHA DE ENTREGA</th>
           <th rowspan="2">FECHA DE RECEPCION</th>
           <th rowspan="2">NUMERO DE ACTA DE ENTREGA</th>
-          <th rowspan="2">FECHA DE REGISTRO ACTUAL</th>
           <th rowspan="2">FECHA DE REGISTRO ACTUAL</th>
           <th rowspan="2">MES DE REGISTRO</th>
           <th rowspan="2">PROCEDENCIA</th>
@@ -31,7 +31,7 @@
         </tr>
       </thead>
       <tbody>
-        <tr v-for="(item, index) in activos_fijos" :key="item.id">
+        <!-- <tr v-for="(item, index) in activos_fijos" :key="item.id">
           <td>{{ index + 1 }}</td>
           <td>{{ item.codigo }}</td>
           <td>{{ obtenerDia(item.fechaAlta) }}</td>
@@ -52,71 +52,137 @@
               </button>
             </div>
           </td>
-        </tr>
+        </tr> -->
         <tr>
           <td></td>
           <td>
             <input
-              v-model="activo_fijo.codigo"
+              v-model="arma.fechaEntrega"
               class="form-control"
-              type="text"
+              type="date"
             />
           </td>
           <td colspan="3">
             <input
-              v-model="activo_fijo.fechaAlta"
+              v-model="arma.fechaRecepcion "
               class="form-control"
               type="date"
             />
           </td>
           <td>
-            <select
-              v-model="activo_fijo.estado"
-              class="form-select"
-              id="estado"
-              name="estado"
-              required
-            >
-              <option value="" selected disabled>seleccionar</option>
-              <option value="Bueno">Bueno</option>
-              <option value="Malo">Malo</option>
-              <option value="Regular">Regular</option>
-            </select>
-          </td>
-          <td>
             <input
-              v-model="activo_fijo.descripcion"
+              v-model="arma.nroActaEntrega "
               class="form-control"
               type="text"
             />
           </td>
           <td>
             <input
-              v-model="activo_fijo.responsable"
+              v-model="arma.fecharegActual"
+              class="form-control"
+              type="date"
+            />
+          </td>
+          <td>
+            <input
+              v-model="arma.mesRegistro"
+              class="form-control"
+              type="date"
+            />
+          </td>
+          <td>
+            <input
+              v-model="arma.procedencia"
               class="form-control"
               type="text"
             />
           </td>
           <td>
-            <select
-              v-model="activo_fijo.tipo"
-              class="form-select"
-              id="tipo"
-              name="tipo"
-              required
-            >
-              <option value="" selected disabled>seleccionar</option>
-              <option value="Equipo de oficina y muebles">
-                Equipo de oficina y muebles
-              </option>
-              <option value="Equipo de Computación">
-                Equipo de Computación
-              </option>
-              <option value="Equipo de comunicación">
-                Equipo de comunicación
-              </option>
-              <option value="Otro tipo">Otro tipo</option>
-            </select>
+            <input
+              v-model="arma.tipoAdquisicion"
+              class="form-control"
+              type="text"
+            />
+          </td>
+          <td>
+            <input
+              v-model="arma.tipoArma"
+              class="form-control"
+              type="text"
+            />
+          </td>
+          <td>
+            <input
+              v-model="arma.marcaArma"
+              class="form-control"
+              type="text"
+            />
+          </td>
+          <td>
+            <input
+              v-model="arma.modelo"
+              class="form-control"
+              type="text"
+            />
+          </td>
+          <td>
+            <input
+              v-model="arma.calibre"
+              class="form-control"
+              type="text"
+            />
+          </td>
+          <td>
+            <input
+              v-model="arma.serialArma"
+              class="form-control"
+              type="text"
+            />
+          </td>
+          <td>
+            <input
+              v-model="arma.industria"
+              class="form-control"
+              type="text"
+            />
+          </td>
+          <td>
+            <input
+              v-model="arma.nroCargador"
+              class="form-control"
+              type="text"
+            />
+          </td>
+          <td>
+            <input
+              v-model="arma.capacidadCargador"
+              class="form-control"
+              type="text"
+            />
+          </td>
+          <td>
+            <input
+              v-model="arma.capacidadTambor"
+              class="form-control"
+              type="text"
+            />
+          </td>
+          <td>
+            <input
+              v-model="arma.estado"
+              class="form-control"
+              type="text"
+            />
+          </td>
+          <td>
+            <input
+              v-model="arma.estadoExistencia"
+              class="form-control"
+              type="date"
+            />
+          </td>
+          <td>
+      
           </td>
           <td>
             <button @click="guardarActivoFijo" class="btn btn-primary">
@@ -133,7 +199,32 @@
 export default{
   data(){  
     return{
-      armas:[]
+      armas:[
+      ],
+      arma:{
+        tipoArma:'',
+        marcaArma:'',
+        modelo:'',
+        calibre:'',
+        serialArma:'',
+        industria:'',
+        nroCargador:'',
+        capacidadCargador:'',
+        capacidadTambor:'',
+        estado:'',
+        descripcion:'',
+        estadoExistencia:'',
+        fechaEntrega:'',
+        fechaRecepcion:'',
+        nroActaEntrega:'',
+        fecharegActual:'',
+        mesRegistro:'',
+        procedencia:'',
+        tipoAdquisicion:'',
+        detallePerdidaId:'',
+
+      }
+
   }
 }
 
